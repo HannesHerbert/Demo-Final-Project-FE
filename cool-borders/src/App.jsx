cb46899d0a15944e8ea8c1a2ba64741895fefc01
Timur <timur.kokumbaev@gmail.com>
2023-03-28 15:46:16 +0200

    Ordnerstruktur erstellt

--- cool-borders/src/App.jsx
+++ cool-borders/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import PrivateRoute from './services/PrivateRoute';
 
 
 
@@ -7,8 +8,28 @@ function App() {
 
 
   return (
-    <div className="App">
-        <h1 className='text-center text-3xl'>Boardergram</h1>
+    <div className="App bg-zinc-800 text-white py-5">
+      <div className='container mx-auto min-h-screen relative  w-5/6'>
+        <h1 className='text-4xl text-center py-9 mb-3 font-bold text-gray-300'>Timur's Todo App</h1>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              {/* <Route index element={}/>
+
+              <Route path='/auth/register' element={} />
+              <Route path='/auth/login' element={} /> */}
+
+              <Route element={<PrivateRoute />}>
+                {/* <Route path='/todolists' element={} />
+                <Route path='/users/user' element={} /> */}
+              </Route>
+            </Route>
+            
+          </Routes>
+        </BrowserRouter>
+
+      </div>
     </div>
   )
 }
