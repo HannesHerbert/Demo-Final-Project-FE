b7037a034f8a9ca3125835d76920c73770cabe67
Sophie Abraham <sophie.abraham@posteo.de>
2023-04-05 16:49:09 +0200

    + added dynamic filter options in Blogs + modified News-Card + added Favs-Card

--- cool-borders/src/pages/Favs.jsx
+++ cool-borders/src/pages/Favs.jsx
@@ -1,13 +1,13 @@
 
 
 
-function Favs () {
+function Favs() {
 
 
     return (
-        <>
-        <button>jdhd</button>
-        </>
+        <div className="bg-black flex flex-col justify-center items-center p-2 w-full h-fit">
+            <h1 className="text-2xl text-orange-600">List of your Favorites</h1>
+        </div>
     )
 }
 
