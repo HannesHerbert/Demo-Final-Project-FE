4b71d11fedc29d7534d6fd6a415bc7403e6ca441
Sophie Abraham <sophie.abraham@posteo.de>
2023-04-05 10:06:42 +0200

    + modified Header component

--- cool-borders/src/components/Header.jsx
+++ cool-borders/src/components/Header.jsx
@@ -2,37 +2,49 @@ import { Link } from "react-router-dom";
 /* import Logo1 from '../assets/LOgoW.png'; */
 import Logo from '../assets/logo2.png';
 
-import{RiLoginBoxLine} from 'react-icons/ri'
+import { RiLoginBoxLine } from 'react-icons/ri'
 
-import{FaUserAlt} from 'react-icons/fa'
+import { FaUserAlt } from 'react-icons/fa'
+import { useState } from "react";
 
 function Header() {
+
+    const [isNews, setIsNews] = useState(isNews);
     
 
+    return (
 
-    return(
-        
-            <header className='fixed top-0 left-0 flex justify-between items-center w-full h-24 '  >
-                <Link to= '/'>
-                    <img src={Logo} alt="logo" width={'90px'}  />
-                   {/*  <img src={Logo} alt="logo" width={'150px'}/> */}
-                </Link>
-                <div className="flex">
-                    <Link to='/user'> <FaUserAlt size="20px" className="mr-5 hover:text-orange-300"/> </Link>
-                    
-                    <Link to='/auth/login' > <RiLoginBoxLine size="22px" className="mr-5 hover:text-orange-300"/></Link>
+        <header className='fixed top-0 left-0 flex justify-between items-center w-full h-24 '  >
+            <Link to='/'>
+                <img src={Logo} alt="logo" width={'90px'} />
+                {/*  <img src={Logo} alt="logo" width={'150px'}/> */}
+            </Link>
 
-                </div>
-                
+            <div>
+                {isNews
+                    ? <Link to='/news' isNews={isNews}> <h2 className='text-5xl font-mono font-black tracking-widest text-orange-500 text-center'>NEWS</h2></Link>
+                    : <Link to='/blogs'> <h2 className='text-5xl font-mono font-black tracking-widest text-orange-500 text-center'>BLOGS</h2></Link>
+                }
+            </div>
             
+            
+
+            <div className="flex">
+                <Link to='/user'> <FaUserAlt size="20px" className="mr-5 hover:text-orange-300" /> </Link>
+
+                <Link to='/auth/login' > <RiLoginBoxLine size="22px" className="mr-5 hover:text-orange-300" /></Link>
+
+            </div>
+
+
             {/*   <div><i className="fa-solid fa-user-plus p-2"></i></div> */}
-    
-            
-                
-            </header>
-       
-  
-        
+
+
+
+        </header>
+
+
+
 
     )
 }
