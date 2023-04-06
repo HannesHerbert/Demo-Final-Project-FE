a7cfcb9a54bbda0af667b33760bb2b68b4f386fb
dci-student <thanhhoa158169@gmail.com>
2023-04-06 14:14:18 +0200

    added user profil

--- cool-borders/src/components/Nav.jsx
+++ cool-borders/src/components/Nav.jsx
@@ -10,12 +10,12 @@ import {Link} from 'react-router-dom';
 function Nav() {
   return (
     <nav className="fixed bottom-0 left-0 w-full h-20 font-mono">
-      <ul className="flex justify-around items-center h-full text-1xl font-bold text-gray-400 bg-gray-900">
+      <ul className="flex justify-around items-center h-full text-xl font-bold text-gray-400 bg-gray-900">
         <li className="hover:text-orange-700 duration-300 ">
           <Link to="/">
             <HiOutlineNewspaper
-              size="30px"
-              className='mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'
+              size="25px"
+              className='mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 '
             />
             <p className="title-nav p-2 text-xs ">News</p>
           </Link>
@@ -23,14 +23,14 @@ function Nav() {
         <li className="hover:text-orange-700 duration-300 ">
           <Link to="/blogs">
             <BsPencilSquare
-              size="30px "
+              size="25px "
               className='mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'
             />
             <p className="title-nav p-2 text-xs">Blog</p>
           </Link>
         </li>
         
-        <li className="  text-orange-700 duration-300 hover:text-amber-600 ">
+        <li className="  text-orange-700 duration-300 hover:text-gray-400 ">
           <Link to='/create'>
    
               <AiOutlinePlusCircle
@@ -45,7 +45,7 @@ function Nav() {
         <li className="hover:text-orange-700 duration-300">
           <Link to="/favs">
             <AiOutlineStar
-              size="30px"
+              size="25px"
               className='mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'
             />
             <p className="title-nav p-2 text-xs">Favs</p>
@@ -54,7 +54,7 @@ function Nav() {
         <li className="hover:text-orange-700 duration-300">
           <Link to="/chat">
             <BsChatText
-              size="30px"
+              size="24px"
               className='mx-auto transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'
             />
             <p className="title-nav p-2 text-xs">Chat</p>
