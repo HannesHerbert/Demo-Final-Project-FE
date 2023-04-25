6129d76aaacd90b4679119a5918d2ddf13fb2c93
Timur <timur.kokumbaev@gmail.com>
2023-04-25 16:28:31 +0200

    users aus useUsersStore gelöscht, und im search mit state gemacht

--- cool-borders/src/store/useUsersStore.js
+++ cool-borders/src/store/useUsersStore.js
@@ -3,25 +3,25 @@ import axios from "axios";
 
 
 const useUsersStore = create((set, get) => ({
-    users: [],
-    searchUsers: async (username) =>{
-        const token = localStorage.getItem('token');
-        try {
-            let resp = await axios.get('http://localhost:8080/protected/searchuser/' + username, {
-              headers: {
-                'Authorization': `Bearer ${token}`
-              }  
-            });
-            console.log(resp.data);
+    // users: [],
+    // searchUsers: async (username) =>{
+    //     const token = localStorage.getItem('token');
+    //     try {
+    //         let resp = await axios.get('http://localhost:8080/protected/searchuser/' + username, {
+    //           headers: {
+    //             'Authorization': `Bearer ${token}`
+    //           }  
+    //         });
+    //         console.log(resp.data);
 
-            set({users: resp.data.users});
+    //         set({users: resp.data.users});
 
-            console.log(users);
-        } catch (error) {
-          console.log(error);
-        } 
-    },
-    setUsers: (users) => set({users: users}),
+    //         console.log(users);
+    //     } catch (error) {
+    //       console.log(error);
+    //     } 
+    // },
+    // setUsers: (users) => set({users: users}),
     
     currUser: null,
     setCurrUser: user => set({currUser: user})
