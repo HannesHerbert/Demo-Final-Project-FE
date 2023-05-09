8719d7e1b0d2b01e0298a10a6c8c0d0381d8e256
Timur <timur.kokumbaev@gmail.com>
2023-05-09 10:36:29 +0200

    Auskommentierte zeile im Header gelöscht

--- cool-borders/src/components/layout/Header.jsx
+++ cool-borders/src/components/layout/Header.jsx
@@ -15,7 +15,6 @@ function Header() {
         <header className='fixed top-0 left-0 flex justify-between items-center w-full h-24 bg-black z-40'  >
 
             <Link to='/' className="flex flex-row justify-center items-center -ml-2 md:-mt-4">
-                {/* <img src={Logo} alt="logo" className="md:mt-16 w-20 md:w-44 opacity-80 rounded-full" /> */}
                 <img src={Logo} alt="logo" className="md:mt-16 w-32 md:w-48 opacity-80 rounded-full p-6" />
             </Link>
 
