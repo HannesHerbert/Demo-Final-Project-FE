818d90818ea65746aa7f419066f93cb961e0c377
Timur <timur.kokumbaev@gmail.com>
2023-05-12 18:14:42 +0200

    Lazy loading angefangen

--- cool-borders/src/pages/News.jsx
+++ cool-borders/src/pages/News.jsx
@@ -9,13 +9,37 @@ function News() {
 
   useEffect(() => {
     fetchNews();
+    console.log(articles);
   }, []);
 
-  async function fetchNews() {
+  // LAZY LOADING....
+  // let options = {
+  //   root: document.querySelector('#layout'),
+  //   rootMargin: '0px',
+  //   threshold: 1.0
+  // }
+
+  // let observer = new IntersectionObserver(fetchNews, options);
+  // let target = <div id="trigger-div" className="w-full h-10"></div>;
+  // console.log(target);
+  // observer.observe(target);
+  // ----------------------------------
+
+  async function fetchNews(entries, observer) {
     try {
-      let resp = await axios.get('http://localhost:8080/public/articles');
+      let resp = await axios.get('http://localhost:8080/public/articles/' + articles?.length);
       // speichere articles in state
-      setArticles(resp.data.data)
+      console.log(resp);
+
+      if (articles.length > 0) {
+        setArticles(prev => [
+          ...prev, 
+          resp.data.data
+        ]);
+      } else {
+        setArticles(resp.data.data)
+      }
+
     } catch (error) {
       console.log(error);
     }
@@ -25,9 +49,11 @@ function News() {
       /* Render News */
       <div className="flex flex-col justify-center items-center p-2 w-full h-fit gap-14">
 
-        {articles.map(article => {
+        {articles?.map(article => {
           return <Post key={article._id} post={article} />
         })}
+
+        <div id="trigger-div" className="w-full h-10"></div>
   
       </div>
   );
