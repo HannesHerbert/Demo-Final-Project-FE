f64d03693541abef92b733e0bcce067615841517
Timur <timur.kokumbaev@gmail.com>
2023-05-13 20:00:50 +0200

    Blogs mit lazy loading und filter versucht zusammenzupassen

--- cool-borders/src/pages/Blogs.jsx
+++ cool-borders/src/pages/Blogs.jsx
@@ -18,12 +18,20 @@ function Blogs() {
 
     // wenn trigger-div inView === true dann fetche neue posts
     useEffect(() => {
+    
+        if (inView && !filter) fetchBlogs(blogs.length);
 
-        if (inView) fetchBlogs();
+    }, [inView]);
 
-    }, [inView, filter]);
+    useEffect(() => {
 
-
+        if (filter.length > 0) {
+            fetchBlogs(0);
+        } else {
+            fetchBlogs(blogs.length);
+        }
+        
+    }, [filter]);
 
     /* Array mit Objekten der Filtermöglichkeiten */
     let optionValues = [
@@ -33,20 +41,31 @@ function Blogs() {
         { label: 'Market', value: 'market' }
     ];
 
-    useEffect(() => {
-        fetchBlogs()
-    }, [filter]);
 
-    async function fetchBlogs() {
+
+    async function fetchBlogs(skip) {
         try {
-            let response = await axios.get('http://localhost:8080/protected/blogs?category=' + filter + '&skip=' + blogs.length, {
+            let response = await axios.get('http://localhost:8080/protected/blogs?category=' + filter + '&skip=' + skip, {
                 headers: {
                     'Authorization': `Bearer ${token}`
                   }  
             });
+            console.log(response.data.data);
+
+            // Check ob blogs hat resp datas
+            let isObjFetched = false;
+
+            blogs.forEach(blog => {
+                response.data.data.forEach(resObj => {
+                    if (blog._id === resObj._id) {
+                        isObjFetched = true;
+                    }
+                })
+
+            });
 
             // speichere blogs
-            if (blogs.length > 0) {
+            if (blogs.length > 0 && !filter && !isObjFetched) {
                 setBlogs([...blogs, ...response.data.data]);
             } else {
                 setBlogs(response.data.data)
@@ -58,8 +77,8 @@ function Blogs() {
     }
 
     return (
-        <div className="flex flex-col justify-center items-center p-2 w-full h-fit gap-14">
-            <div className="self-end">
+        <div className="relative flex flex-col justify-center items-center p-2 w-full h-fit gap-14">
+            <div className="self-end sticky top-20 right-5">
                 <select onChange={e => setFilter(e.target.value)} className="p-1 rounded-md text-white bg-black hover:text-indigo-200 mt-6">
 
                     {optionValues.map((filter) => (
